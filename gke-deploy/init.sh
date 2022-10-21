a43e6e84a3e644f8a024bee8a5f99169165b7756
evercurious <34236239+vishnu-anil@users.noreply.github.com>
2022-10-21 13:24:12 +0530

    Create init.sh

---
+++ gke-deploy/init.sh
@@ -0,0 +1,21 @@
+          #!/bin/bash
+          cd /
+          set -x
+          whoami 
+          apt-get update
+          apt install wget git -y
+          wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64 \
+          && chmod +x yq_linux_amd64 \
+          && mv yq_linux_amd64 yq \
+          && cp yq /usr/local/bin
+          curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 \
+          && chmod 700 get_helm.sh \
+          && ./get_helm.sh
+          wget -O helmfile_linux_amd64 https://github.com/roboll/helmfile/releases/download/v0.135.0/helmfile_linux_amd64
+          chmod +x helmfile_linux_amd64
+          mv helmfile_linux_amd64 /bin/helmfile
+          apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y 
+          apt-get install kubectl -y 
+          export KUBECONFIG=$HOME/.kube/config
+          gcloud container clusters get-credentials $CLUSTER_NAME --region $REGION
+      
